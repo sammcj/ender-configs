3c45bc1584b9031952aec61b7ddaaa264494942f
Sam McLeod <sammcj@users.noreply.github.com>
2023-05-05 08:36:03 +1000

    update scripts

--- scripts/generate-resonance-graphs.sh
+++ scripts/generate-resonance-graphs.sh
@@ -1,68 +1,94 @@
 #!/usr/bin/env bash
 
-set -ex
-
 # Set date variable
 DATE=$(date +%Y-%m-%d)
-
-PYTHON_BIN="${HOME}/klippy-env/bin/python3"
-GRAPH_SCRIPT="${HOME}/klipper/scripts/calibrate_shaper.py"
-RUN_COMMAND="${PYTHON_BIN} ${GRAPH_SCRIPT}"
-
-# Set destination directory
+PYTHON_BIN=$HOME/klippy-env/bin/python3
+GRAPH_SCRIPT=${HOME}/klipper/scripts/calibrate_shaper.py
+RUN_COMMAND="$PYTHON_BIN $GRAPH_SCRIPT"
 DEST_DIR="/home/octo/calibration"
 DEST_DIR_DATE="${DEST_DIR}/${DATE}"
+RESONANCES_LOG="${DEST_DIR_DATE}/resonances-${DATE}.log"
+FILENAME=$(basename "$file")
+FILENAME_NO_EXT="${FILENAME%.*}"
+DEBUG=${DEBUG:-false}
+INTERACTIVE=${INTERACTIVE:-true}
 
-# Create a new directory in the destination directory
-mkdir -p "${DEST_DIR_DATE}/csv"
+if [[ "$DEBUG" = true ]]; then
+  echo "Debug enabled"
+  set -x
+fi
 
-# Log files
-CALIBRATION_LOG="${DEST_DIR_DATE}/run.log"
-RECOMMENDATIONS="${DEST_DIR_DATE}/recommendations.txt"
+mkdir -p "$DEST_DIR_DATE" && cd "$DEST_DIR_DATE" || exit
 
-cd "$DEST_DIR_DATE"
+# check if RESONANCES_LOG exists, if so move them to .old and create new ones
+if [ -f "$RESONANCES_LOG" ]; then
+  mv "$RESONANCES_LOG" "$RESONANCES_LOG.old"
+fi
 
-touch "$CALIBRATION_LOG"
-touch "$RECOMMENDATIONS"
+touch "$RESONANCES_LOG"
 
-# Copy all .csv files created today in /tmp/ to $DEST_DIR/$DATE/csv/
-find /tmp/ -maxdepth 1 -type f -name "*.csv" -newermt "${DATE} 00:00:00" ! -newermt "${DATE} 23:59:59" -exec cp {} "${DEST_DIR_DATE}/csv/" \;
+# Copy all .csv files created today in /tmp/ to $DEST_DIR/$DATE/
+find /tmp/ -maxdepth 1 -type f -name "*.csv" -newermt "${DATE} 00:00:00" ! -newermt "${DATE} 23:59:59" -exec cp {} "${DEST_DIR_DATE}/" \;
 
-# Loop over each csv file in $DEST_DIR/$DATE/csv/ and run calibrate_shaper.py on it
-for file in "${DEST_DIR_DATE}/csv/"*.csv; do
+# Loop over each csv file in $DEST_DIR/$DATE/ and run calibrate_shaper.py on it
+for file in "$DEST_DIR_DATE"/*.csv; do
   eval "$(
-    FILENAME=$(basename "$file")
-    FILENAME_NO_EXT="${FILENAME%.*}"
-    FILENAME_LOG="${FILENAME_NO_EXT}.log"
     output=$(
 
-      # # skip the files if the png already exists
-      # if [ -f "${FILENAME_NO_EXT}-${DATE}.png" ]; then
-      #   echo "Skipping ${FILENAME_NO_EXT} as ${FILENAME_NO_EXT}-${DATE}.png already exists"
-      # else
-
-      $RUN_COMMAND "$file" -o "${FILENAME_NO_EXT}-${DATE}.png" >>"${FILENAME_LOG}" &
-
-      # grep for the line that contains "recommended shaper" and get axis from the filename and echo them
-      RECOMMENDED_SHAPER=$(grep "recommended shaper" "${FILENAME_LOG}" | tee -a "$FILENAME_LOG")
-
-      echo -e "Axis file: $FILENAME_LOG \n - $RECOMMENDED_SHAPER \n" >>"$RECOMMENDATIONS"
-
-      # fi
+      # skip the files if the png already exists
+      if [ -f "${FILENAME_NO_EXT}-${DATE}.png" ]; then
+        echo "${FILENAME_NO_EXT} as ${FILENAME_NO_EXT}-${DATE}.png already exists, skipping..."
+      else
+        eval $RUN_COMMAND "$file" -o "${FILENAME_NO_EXT}-${DATE}.png" &
+      fi
     )
 
-    echo "$output" >>"$CALIBRATION_LOG"
-  )" &
+    echo "$output" >>"$RESONANCES_LOG"
+  )"
 done
 
+echo "Waiting for background processes to finish..."
 wait # wait for all background processes to finish
 
+for file in "$DEST_DIR_DATE"/resonances_*.log; do
+  # grep for the line that contains "recommended shaper" and get axis from the filename and echo them
+  RECOMMENDED_SHAPER=$(
+    grep -i 'Recommended shaper' "$file"
+  )
+
+  SHAPER=$(
+    echo "$RECOMMENDED_SHAPER" | cut -d " " -f 4
+  )
+
+  # Find the max accel (the string after 'Recommended shaper' in $RECOMMENDED_SHAPER) and echo it
+  RECOMMENDED_ACCEL=$(
+    grep -i "To avoid too much smoothing with '$SHAPER'" "$file"
+  )
+
+  # Append the recommended shaper and accel to the log file
+  echo -e "$file \n - $RECOMMENDED_SHAPER \n" | tee -a "$RESONANCES_LOG"
+  echo -e "$file \n - $RECOMMENDED_ACCEL \n" | tee -a "$RESONANCES_LOG"
+done
+
 # Cleanup
-rm -rf "${DEST_DIR_DATE}/csv"
+rm -rf "$DEST_DIR_DATE"/*.csv
+ls -la "${DEST_DIR_DATE}/" >>"$RESONANCES_LOG"
 
-ls -la "${DEST_DIR_DATE}/" >>"$CALIBRATION_LOG"
+echo "This output was generated from generate-resonance-graphs.sh - https://github.com/sammcj/ender-configs/blob/main/scripts/generate-resonance-graphs.sh " >>"$RESONANCES_LOG"
 
-echo "You can now run the following command to copy the graphs from your local machine:" >>"$CALIBRATION_LOG"
-echo "rsync -avr root@rockpi:${DEST_DIR_DATE} /Users/samm/git/sammcj/ender-configs/ender-5-s1/calibration/ --partial --progress" >>"$CALIBRATION_LOG"
+echo "Completed."
 
-cat "$CALIBRATION_LOG"
+if [[ "$INTERACTIVE" = true ]]; then
+  echo "Would you like to add a note to the recommendations log? (y/n)"
+  read -r COMMENT
+  if [ "$COMMENT" = "y" ]; then
+    echo "Enter your note:"
+    read -r COMMENT
+    echo "${DATE} - ${COMMENT}" >>"$RESONANCES_LOG"
+  fi
+fi
+
+echo "Log written to ${RESONANCES_LOG}"
+echo "---"
+echo "You can rsync these back to your local machine, e.g."
+echo "rsync -avr root@rockpi:${DEST_DIR_DATE} /Users/samm/git/sammcj/ender-configs/ender-5-s1/calibration/ --partial --progress"
